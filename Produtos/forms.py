94ed251cb3fe73a853aafebec59d62747249d5a0
JoaoManoelFontes <jmanufontesc@gmail.com>
2023-07-24 15:38:43 -0300

    add form page

--- Produtos/forms.py
+++ Produtos/forms.py
@@ -10,8 +10,10 @@ class FilmeForm(ModelForm):
         fields = '__all__'
         widgets = {
             'nome': forms.TextInput(attrs={'class': 'form-control'}),
-            'sinopse': forms.Textarea(attrs={'class': 'form-control'}),
-            'elenco': forms.Textarea(attrs={'class': 'form-control'}),
+            'sinopse': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+            'elenco': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
             'preco': forms.NumberInput(attrs={'class': 'form-control'}),
+            'imagem': forms.FileInput(attrs={'class': 'form-control'}),
+            'poster': forms.FileInput(attrs={'class': 'form-control'}),
             'trailer': forms.URLInput(attrs={'class': 'form-control'}),
         }
